2ca00dbc45f8256abc0313760885835e6bf84465
aakash chougule <aakashrorosing@gmail.com>
2026-04-15 17:00:03 +0530

    updated the dp.php for deployment

--- db.php
+++ db.php
@@ -1,5 +1,10 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "weather_app");
+$host = "sql100.infinityfree.com";
+$user = "if0_41667748";
+$password = "aakashrorosing1";
+$database = "if0_41667748_weatherdb";
+
+$conn = new mysqli($host, $user, $password, $database);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
